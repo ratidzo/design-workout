ee76a28140908b877aab90be1c77c00fb5f52196
Raty <ratygore@gmail.com>
2025-01-14 23:18:37 +0200

    dev: demo objective

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,11 @@ const navigation = [
 
 export default function Home() {
   return (
-    <div className="flex w-screen justify-center">
+    <div className="flex w-screen h-screen justify-center items-center">
       <ul>
         {
           navigation.map(item => (
-            <li key={item.name}>
+            <li key={item.name} className="py-2 hover:text-blue-400 transition-all duration-200">
               <Link href={item.href}>
                 {item.name}
               </Link>
---
+++ app/shrink/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Page() {
+  return (
+    <div className="flex w-screen h-screen justify-center items-center gap-4 px-4">
+
+      <div className="flex-none w-40 h-20 bg-blue-400 rounded"></div>
+      <div className="shrink w-40 h-20 bg-pink-400 rounded"></div>
+      <div className="flex-none w-40 h-20 bg-green-400 rounded"></div>
+
+    </div>
+  )
+}
